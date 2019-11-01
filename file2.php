3d1e69ee2494ccf96c80db06c8ce205bf2255908
huzaifa aslam <huzaifaslam45@gmail.com>
2019-11-02 00:04:34 +0500

    file1.php and file2.php and file3.php added

---
+++ file2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php echo "hello baby";?>
+	<?php echo "hello world";?>
+	<?php echo "hello world";?>
+</html>
---
+++ file3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php echo "hello class";?>
+	<?php echo "hello world";?>
+	<?php echo "hello world";?>
+</body>
+</html>
